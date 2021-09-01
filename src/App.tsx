10a05001ac4a90b27f127fed65d92fa4603463c6
Jerry Jones <jerryjfernandes@gmail.com>
2021-09-01 12:47:09 -0300

    feat: add app page

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,32 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react';
+import { DisplayUser } from './components/DisplayUser';
+import { SearchBar } from './components/SearchBar';
+import { Button } from './components/Button';
+import { DisplayRepoInfos } from './components/DisplayRepoInfos';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import * as S from './styles'
+import { GlobalStyle } from './styles/global';
+
+const userFake = {
+    avatar_url: 'https://www.techcentral.ie/wp-content/uploads/2019/11/GitHub-Mark.png', 
+    login: '---', 
+    name: '---'
 }
 
-export default App;
+export function App () {
+    const [ user, setUser ] = useState<GitUser>(userFake)
+    const [ repos, setRepos ] = useState<Repository[]>([])
+    const [ starred, setStarred ] = useState<Starred[]>([])
+
+    return (
+        <S.Container>
+            <S.Title>Git Search User</S.Title>
+            <DisplayUser user={user} />
+            <SearchBar setUser={setUser} setRepos={setRepos} setStarred={setStarred} />
+            <Button username={user.login} endpoint="repos" setInfo={setRepos}>Repos</Button>
+            <Button username={user.login} endpoint="starred" setInfo={setStarred}>Starred</Button>
+            <DisplayRepoInfos repos={repos} starred={starred} />
+            <GlobalStyle />
+        </S.Container>
+    )
+}
