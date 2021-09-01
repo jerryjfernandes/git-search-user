9687e3e635f26b0cd55d9c9d6d513b67a7aa3078
Jerry Jones <jerryjfernandes@gmail.com>
2021-09-01 12:43:34 -0300

    chore: add types to app

---
+++ src/types.d.ts
@@ -0,0 +1,51 @@
+interface User {
+    avatar_url: string;
+    login: string;
+    name: string;
+    html_url?: string;
+}
+
+interface GitUser {
+    avatar_url: string;
+    bio?: string;
+    blog?: string;
+    company?: string | null;
+    created_at?: string;
+    email?: string | null;
+    events_url?: string;
+    followers?: number;
+    followers_url?: string;
+    following?: number;
+    following_url?: string;
+    gists_url?: string;
+    gravatar_id?: string;
+    hireable?: boolean;
+    html_url?: string;
+    id?: number;
+    location?: string;
+    login: string;
+    name: string;
+    node_id?: string;
+    organizations_url?: string;
+    public_gists?: number;
+    public_repos?: number;
+    received_events_url?: string;
+    repos_url?: string;
+    site_admin?: boolean;
+    starred_url?: string;
+    subscriptions_url?: string;
+    twitter_username?: string | null;
+    type?: string;
+    updated_at?: string;
+    url?: string;
+}
+
+interface Repository {
+    id: number;
+    name: string;
+}
+
+interface Starred {
+    id: number;
+    full_name: string;
+}
